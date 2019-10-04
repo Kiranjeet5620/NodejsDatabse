baaff65e339852570a1fbd0e30a2e1b9f547a854
kiranKverma <kiranjeet5620@gmail.com>
2019-10-04 14:02:33 -0400

    commit

--- database.js
+++ database.js
@@ -24,23 +24,31 @@ db.run('CREATE TABLE enrollment (enrollmentid VARCHAR(20),studentid VARCHAR(20),
   }
   console.log('enrollment Table created')
 })
-db.run('insert into students (studentid, studentname, programgroup) values (741277,Kiran,CSAT);', function (err) {
+db.run("insert into students (studentid, studentname, programgroup) values (741277,'Kiran','CSAT'),(744678,'Aman','CSAT'),(743540,'Sonia','CSAT');", function (err) {
   if (err) {
     return console.log(err.message)
   }
   console.log('student inserted')
 })
-db.run('insert into classes (classid, classname, room, datesession) values (CSD3314_4,WebTech,B230,THU-AM);', function (err) {
+db.run("insert into classes (classid, classname, room, datesession) values ('CSD3314_4','WebTech','B230','THU-AM'),('CSD1234_4','JAVA','B123','FRI-AM');", function (err) {
   if (err) {
     return console.log(err.message)
   }
   console.log('class inserted')
 })
 
-db.run('insert into enrollment (enrollmentid, studentid, classid) values (1233,741277,CSD3314_4);', function (err) {
+db.run("insert into enrollment (enrollmentid, studentid, classid) values (1233,741277,'CSD3314_4'),(1234,744678,'CSD3314_4'),(1235,743540,'CSD3314_4');", function (err) {
   if (err) {
     return console.log(err.message)
   }
-  console.log('enrollment enterred')
+  console.log('enrolled')
+})
+db.serialize(() => {
+  db.each("Select enrollment.studentid,studentname,programgroup, classname, room, datesession from students,classes,enrollment where enrollment.classid='CSD3314_4'", (err, row) => {
+    if (err) {
+      throw err
+    }
+    console.log(row.studentid, row.studentname, row.programgroup, row.classname, row.room, row.datesession)
+  })
 })
 db.close()
