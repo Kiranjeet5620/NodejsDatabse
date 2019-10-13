766b8f6c72722bae0f637ffb625d4b18db223859
kiranKverma <kiranjeet5620@gmail.com>
2019-10-13 16:20:08 -0400

    --- Auto-Git Commit ---

--- 2019.10.04.FRI/orange.js
+++
@@ -1,3 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-let db= new sqlite3.Database('/user.db')
-db.run('CREATE TABLE users(id INTEGER ))
--- 2019.10.04.FRI/tangerine.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app= express();
-app.get('/',function(req,res){
-res.send('ready Dog cat kutee billian//')
-})
-app.listen(9027,function(){
-    console.log('Server ready2');
-})
